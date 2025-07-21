4c4695c9ddb37e742e943f0fe50def4a67a082a2
shailendravermaa <sv8576318@gmail.com>
2025-07-21 09:17:32 +0530

    added correct base url to frontend

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://byte-battle-eteo4sl8j-shailendra-s-projects-99ab3c11.vercel.app',
+    baseURL: 'https://byte-battle-two.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
